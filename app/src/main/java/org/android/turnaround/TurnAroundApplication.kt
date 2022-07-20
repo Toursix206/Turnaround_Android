0f0bde3a85bea7795b158924fa1e94dba2762db9
안나영 <dpswpfskdud@naver.com>
2022-07-20 16:44:18 +0900

    [feat] 카카로 로그인 api 초기 세팅 및 앱키 연동

--- app/src/main/java/org/android/turnaround/TurnAroundApplication.kt
+++ app/src/main/java/org/android/turnaround/TurnAroundApplication.kt
@@ -1,11 +1,12 @@
 package org.android.turnaround
 
 import android.app.Application
+import com.kakao.sdk.common.KakaoSdk
 
-class TurnAroundApplication :Application(){
+class TurnAroundApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        
+        KakaoSdk.init(this, BuildConfig.NATIVE_APP_KEY)
     }
 }
