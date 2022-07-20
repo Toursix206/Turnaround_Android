81ad5ea75f034d2bbef5d71ccda6a0d83b0e3c95
안나영 <dpswpfskdud@naver.com>
2022-07-20 15:25:23 +0900

    [chore] Application 클래스 생성 (#1)

---
+++ app/src/main/java/org/android/turnaround/TurnAroundApplication.kt
@@ -0,0 +1,11 @@
+package org.android.turnaround
+
+import android.app.Application
+
+class TurnAroundApplication :Application(){
+
+    override fun onCreate() {
+        super.onCreate()
+        
+    }
+}
