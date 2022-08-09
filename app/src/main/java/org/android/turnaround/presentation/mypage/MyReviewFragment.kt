347c5c7e5d6eb0c18b320457058e259a8da25dc9
안나영 <dpswpfskdud@naver.com>
2022-08-09 15:52:00 +0900

    [feat] 마이페이지 나의 리뷰 디자인 구현 (#20)

---
+++ app/src/main/java/org/android/turnaround/presentation/mypage/MyReviewFragment.kt
@@ -0,0 +1,23 @@
+package org.android.turnaround.presentation.mypage
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import dagger.hilt.android.AndroidEntryPoint
+import org.android.turnaround.R
+import org.android.turnaround.databinding.FragmentMyReviewBinding
+import org.android.turnaround.presentation.base.BaseFragment
+
+@AndroidEntryPoint
+class MyReviewFragment : BaseFragment<FragmentMyReviewBinding>(R.layout.fragment_my_page) {
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+
+}
