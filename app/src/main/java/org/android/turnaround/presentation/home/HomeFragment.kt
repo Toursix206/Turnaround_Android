d4892b10ba2000009e831ff85da23c957fad9d40
minWachya <alal7272@naver.com>
2022-08-25 15:29:11 +0900

    [feat] roomsetting step2 scroll 추가

--- app/src/main/java/org/android/turnaround/presentation/home/HomeFragment.kt
+++ app/src/main/java/org/android/turnaround/presentation/home/HomeFragment.kt
@@ -35,7 +35,6 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        
         setTodoAdapter()
         setBannerAdapter()
         setKitAdapter()
